499f95e632f488ac4f877a51552890f69af95531
dmitry-ivkin <55837705+dmitry-ivkin@users.noreply.github.com>
2019-12-05 13:58:29 +0300

    Add files via upload

---
+++ lab2-2/src/com/company/Tester.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.Scanner;
+import com.company.Circle;
+
+public class Tester {
+    public static void main(String[] args) {
+        double r;
+        Circle k1 = new Circle(3.1, 4.1, 5.1, "red");
+        System.out.println("Длина окружности = " + k1.getLength() + "см\n");
+        Scanner source = new Scanner(System.in);
+        System.out.println("Введите радиус ");
+        r = source.nextDouble();
+        k1.setR(r);
+        System.out.println("\nДлина окружности = " + k1.getLength() + "см");
+    }
+}
